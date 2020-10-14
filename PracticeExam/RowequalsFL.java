9795133195540cb60e0c06be1d0271633fc42013
Meri Khurshudyan <merikhurshudyan52@gmail.com>
2020-10-14 14:23:40 -0400

    Update RowequalsFL.java

--- PracticeExam/RowequalsFL.java
+++ PracticeExam/RowequalsFL.java
@@ -1,11 +1,14 @@
 //is sum of diagonal equal to sum of a row?
 public class RowequalsFL {
     public static void main(String[] args){
-        int[][] myArray = {{1,1,1},{1,1,1},{1,1,1}};
+        int[][] myArray = {{1,1,1},{1,1,1}};
         System.out.println(equate(myArray,1));
     }
 
     public static boolean equate(int[][] arr, int row){
+        if(arr.length != arr[1].length){
+            throw new IllegalArgumentException("Not a square array.");
+        }
         boolean y = false;
         int sumDiag = 0;
         int sumRow = 0;
