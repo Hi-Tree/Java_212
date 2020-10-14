1cb17b1bb1faf1b9376c3b16b8bfd2563210940e
Meri Khurshudyan <merikhurshudyan52@gmail.com>
2020-10-14 16:32:50 -0400

    Create Question3.java

---
+++ ActualExam1/Question3.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+public class Question3 {
+    public static void main(String[] args){
+        String[] arr = new String[100];
+        int k = 0;
+        int odds = 0;
+        while(true){
+            String dial = JOptionPane.showInputDialog(null, "Enter One string at a time: ");
+            if(dial.equals("STOP")){
+                int i = 0;
+                    while(arr[i] != null){
+                    if(arr[i].length() % 2 != 0){
+                        odds++;
+                    }
+                    i++;
+                }
+                JOptionPane.showMessageDialog(null, "Numbr of odd length words: " + odds);
+                System.exit(0);
+            }
+        
+            else{
+                arr[k] = dial;
+                k++;
+            }
+        }
+        }
+    }
+
