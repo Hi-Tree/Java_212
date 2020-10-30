c0b4c562de448567be7f75a069928e454a9a4d99
Meri Khurshudyan <merikhurshudyan52@gmail.com>
2020-10-30 15:10:31 -0400

    Create ClassTest.java

---
+++ ActualExam1/ClassTest.java
@@ -0,0 +1,7 @@
+public class ClassTest {
+    public static void main(String[] args){
+    Pen cross = new Pen("black", 3.0f);
+    cross.setPrice(-10.0f);
+    System.out.println(cross.getPrice());
+    }
+}
